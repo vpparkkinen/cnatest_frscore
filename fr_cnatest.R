a46fde00f62da7ef05dc8e5ba2ae5c6e8af46279
Veli-Pekka Parkkinen <parkkinenv@gmail.com>
2024-04-15 13:20:12 +0000

    increase N

--- fr_cnatest.R
+++ fr_cnatest.R
@@ -10,6 +10,7 @@ source("cnaTest.R")
 library(doParallel)
 library(frscore)
 n_cores <- detectCores() - 2
+options(mc.cores = n_cores)
 
 flipout <- function(data, outcome, proportion){
   N <- nrow(data)
@@ -27,29 +28,51 @@ flipout <- function(data, outcome, proportion){
   return(data)
 }
 
-n_models <- 10
+bis_submodel <- function(x,y){
+  if (is.na(x) || is.null(x)){
+    NA
+  } else {
+    is.submodel(x, y)
+  }
+}
+
+rowdup <- function(data, times){
+  do.call(rbind, replicate(times, data, simplify = FALSE))
+}
+
+set.seed(22)
+n_models <- 200
 n_factors <- 6
-noise_prop <- 0.3
+noise_prop <- 0.25
+#row_multip <- 1 # duplicate rows this many times
+#                # 1 = no duplication
+N <- 40
+
 targets <- replicate(n_models, randomAsf(n_factors), simplify = FALSE)
 outcomes <- lapply(targets, rhs)
 
-clean_dsets <- lapply(targets, function(x) ct2df(selectCases(x)))
-noisy_dsets <- mapply(flipout, 
-                      data = clean_dsets, 
+clean_dsets <- mclapply(targets,
+                      function(x) cna::some(ct2df(selectCases(x)), N))
+
+
+
+
+noisy_dsets <- mcmapply(flipout,
+                      data = clean_dsets,
                       outcome = outcomes,
-                      proportion = noise_prop, 
+                      proportion = noise_prop,
                       SIMPLIFY = FALSE)
 
-pvals_temp <- mcmapply(cnaTest, 
-                     d = noisy_dsets, 
-                     outcome = outcomes, 
+pvals_temp <- mcmapply(cnaTest,
+                     d = noisy_dsets,
+                     outcome = outcomes,
                      SIMPLIFY = FALSE)
 
 pvals <- unlist(lapply(pvals_temp, `[[`, 8))
 
 sig <- which(pvals < 0.05)
 
-frscore_results <- mcmapply(frscored_cna, 
+frscore_results <- mcmapply(frscored_cna,
                             x = noisy_dsets,
                             outcome = outcomes,
                             output = "asf",
@@ -57,17 +80,16 @@ frscore_results <- mcmapply(frscored_cna,
                             SIMPLIFY = FALSE)
 
 top_frscore <- lapply(frscore_results, function(x) x[[1]][1,2])
-correctness <- mcmapply(is.submodel, 
-                        x = top_frscore, 
+correctness <- mcmapply(bis_submodel,
+                        x = top_frscore,
                         y = targets,
                         SIMPLIFY = FALSE)
 
 correctness <- unlist(correctness)
-cor_all_avg <- sum(correctness) / n_models
+cor_all_avg <- sum(correctness, na.rm = TRUE) / n_models
 
-sig_correctness <- sum(correctness[sig]) / length(sig)
+sig_correctness <- sum(correctness[sig], na.rm = TRUE) / length(sig)
 
 cor_all_avg
 sig_correctness
-
-
+length(sig)
